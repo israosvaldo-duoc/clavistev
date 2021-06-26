5bc378c1c8f48663852cc99a5754b364cf8d43da
Israel Acuña Uval <isra.acuna@duocuc.cl>
2021-06-26 19:42:29 -0400

    Creo Template listado_celulares y configuracion

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, listado_celulares
 
 urlpatterns = [
     path('', home, name="home"),
+    path('listado-celulares/', listado_celulares, name="listado_celulares")
 ]
