bece3b2ce1beb9c8a1d1c283b8f4b1b88a39be97
Israel Acuña Uval <isra.acuna@duocuc.cl>
2021-06-26 22:16:02 -0400

    Programación listado_celulares en views.py y en html

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
+from core.models import Celular
 from django.shortcuts import render
+from .models import Celular
 
 # Create your views here.
 
@@ -6,7 +8,14 @@ def home(request):
     return render(request, 'core/home.html')
 
 def listado_celulares(request):
-    return render(request, 'core/listado_celulares.html')
+    # Creamos una variable que traiga el listado de celulares desde la BD
+    celulares = Celular.objects.all()
+    # Creamos un objeto data que traspasará los datos para utilizarlos en el html asociado
+    data = {
+        'celulares': celulares
+    }
+    # Traspasamos data como tercer parámetro al momento de randerizar 
+    return render(request, 'core/listado_celulares.html', data)
 
 def crear_celular(request):
     return render(request, 'core/crear_celular.html')
