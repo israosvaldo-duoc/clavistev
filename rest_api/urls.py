0fe91abfba226b6f44abd1a78fafe852a2a3e751
Mariapaz Silva <mariap.silvag@duocuc.cl>
2021-07-11 23:31:18 -0400

    Creando api parte 2

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import  celular_1, celular_2
 
 urlpatterns = [
-    path('celular/', celular_1, name='celular_1'),
-    path('celular/<pk>', celular_2, name='celular_2')
+    path('celular/', celular_1, name="celular_1"),
+    path('celular/<int:pk>', celular_2, name="celular_2")
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -4,8 +4,11 @@ from core.models import Celular
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import serializers, status
+from rest_framework.decorators import api_view
 # Create your views here.
 
+# Crud API (Completa) parte 1.
+@api_view(['GET','POST'])
 def celular_1(request):
     # Permite listar todos los celulares
     if request.method == 'GET':
@@ -20,6 +23,30 @@ def celular_1(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)     
 
-#def celular_2(request):    
+# Crud API (Completa) parte 2.
+@api_view(['GET','PUT','DELETE'])
+def celular_2(request, pk):
+        # Instanciar el elemento singular desde la bd
+        try:
+            celular = Celular.objects.get(idCelular=pk)
+        except Celular.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # Permite retornar un elemento desde la bd
+        if request.method == 'GET':
+            serializer = CelularSerializer(celular)
+            return Response(serializer.data)
+        # Permite actualizar un elemento desde la bd
+        if request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = CelularSerializer(celular, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+        # Permite eliminar un elemento desde la bd
+        elif request.method == 'DELETE':
+            celular.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
